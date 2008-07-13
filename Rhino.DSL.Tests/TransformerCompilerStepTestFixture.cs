ae29391eced76660d987f978318d22a9e78a69bd
nrstott <nrstott@079b0acf-d9fa-0310-9935-e5ade295c882>
2008-07-13 16:49:33 +0000

    Added TransformerCompilerStep, a compiler step for applying a set of DepthFirstTransformers to the CompileUnit.
    git-svn-id: https://rhino-tools.svn.sourceforge.net/svnroot/rhino-tools/trunk@1571 079b0acf-d9fa-0310-9935-e5ade295c882

---
+++ Rhino.DSL.Tests/TransformerCompilerStepTestFixture.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Boo.Lang.Compiler;
+using Boo.Lang.Compiler.Ast;
+using MbUnit.Framework;
+
+namespace Rhino.DSL.Tests
+{
+	[TestFixture]
+	public class TransformerCompilerStepTestFixture
+	{
+		[Test]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void CtorRequiresNonNullArgument()
+		{
+			new TransformerCompilerStep(null);
+		}
+
+		[Test]
+		[ExpectedException(typeof(ArgumentException))]
+		public void CtorRequiresAtLeastOneTransformer()
+		{
+			new TransformerCompilerStep(new DepthFirstTransformer[] {});
+		}
+
+		[Test]
+		public void TransformerIsAppliedToCompileUnit()
+		{
+			StubTransformer transformer = new StubTransformer();
+
+			TransformerCompilerStep transformerStep = new TransformerCompilerStep(transformer);
+
+			CompileUnit unit = new CompileUnit();
+
+			BooCompiler compiler = new BooCompiler();
+			compiler.Parameters.Pipeline = new CompilerPipeline();
+			compiler.Parameters.Pipeline.Insert(0, transformerStep);
+			compiler.Run(unit);
+
+			Assert.IsTrue(transformer.CompileUnitVisited);
+		}
+
+		private class StubTransformer: DepthFirstTransformer
+		{
+			private bool compileUnitVisited = false;
+
+			public bool CompileUnitVisited
+			{
+				get { return compileUnitVisited; }
+			}
+
+			public override void OnCompileUnit(CompileUnit node)
+			{
+				compileUnitVisited = true;
+				base.OnCompileUnit(node);
+			}
+		}
+	}
+}
---
+++ Rhino.DSL/TransformerCompilerStep.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Boo.Lang.Compiler.Ast;
+using Boo.Lang.Compiler.Steps;
+
+namespace Rhino.DSL
+{
+	/// <summary>
+	/// Compiler step to run <see cref="DepthFirstTransformer"/>s.
+	/// </summary>
+	/// <remarks>
+	/// This is a useful way to apply one or more transformers as a compiler step.
+	/// </remarks>
+	public class TransformerCompilerStep : AbstractVisitorCompilerStep
+	{
+		/// <summary>
+		/// Creates an instance of TransformerCompilerStep.
+		/// </summary>
+		/// <param name="transformers">Transformers for this compiler step to run.</param>
+		/// <exception cref="ArgumentNullException"><paramref name="transformers"/> should not be null.</exception>
+		/// <exception cref="ArgumentException"><paramref name="transformers"/> should have at least one element.</exception>
+		public TransformerCompilerStep(params DepthFirstTransformer[] transformers)
+		{
+			if (transformers == null)
+				throw new ArgumentNullException("transformers");
+			if (transformers.Length == 0)
+				throw new ArgumentException("Expected at least one element.", "transformers");
+
+			this.transformers = transformers;
+		}
+
+		/// <summary>
+		/// Run the <see cref="TransformerCompilerStep"/>.
+		/// </summary>
+		public override void Run()
+		{
+			foreach (DepthFirstTransformer transformer in transformers)
+				transformer.Visit(CompileUnit);
+		}
+
+		private readonly DepthFirstTransformer[] transformers;
+	}
+}
